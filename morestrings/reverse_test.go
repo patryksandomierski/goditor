7826c39122507e4421f96c8784ddc0bac0460de1
psandomierski <psandomierski@sii.pl>
2020-11-13 13:38:15 +0100

    Add data driven test

--- morestrings/reverse_test.go
+++ morestrings/reverse_test.go
@@ -1,6 +1,7 @@
 package morestrings
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -9,5 +10,26 @@ func TestIntMin(t *testing.T) {
 	expected := "cba"
 	if ans != expected {
 		t.Errorf("expected: %s\n got: %s", expected, ans)
+	}
+}
+
+func TestIntMinDataDriver(t *testing.T) {
+	var tests = []struct {
+		a        string
+		expected string
+	}{
+		{"asd", "dsa"},
+		{"123456", "654321"},
+		{"aaaaaa", "aaaaaa"},
+	}
+
+	for _, td := range tests {
+		testname := fmt.Sprintf("%s,%s", td.a, td.expected)
+		t.Run(testname, func(t *testing.T) {
+			ans := ReverseChars(td.a)
+			if ans != td.expected {
+				t.Errorf("expected: %s\n got: %s", td.expected, ans)
+			}
+		})
 	}
 }
